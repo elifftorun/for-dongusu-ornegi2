726762732b4444f86c615f956202128e788ab376
elifftorun <145452036+elifftorun@users.noreply.github.com>
2023-10-23 14:38:05 +0300

    Add files via upload

---
+++ for-dongu-ornegi2/for-dongu-ornegi2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_dongu_ornegi2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int topla, sayi1, sayi2;
+            topla = 0;
+            sayi1 =Convert.ToInt32(textBox1.Text);
+            sayi2 =Convert.ToInt32(textBox2.Text);
+            for(int i=sayi1; i <= sayi2; ++i)
+            {
+                topla += i;
+            }
+            label3.Text=topla.ToString();
+        }
+    }
+}
